3b4aa032207c819b36e089db3f4d8b408f3b8a9b
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-02-24 11:51:27 +0200

    ппп

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,35 @@
-import AppBar from "../AppBar/AppBar.jsx";
-import officers from "../../officers.json";
-import PilotList from "../PilotList/PilotList.jsx";
-import clsx from "clsx";
+// import AppBar from "../AppBar/AppBar.jsx";
+// import officers from "../../officers.json";
+// import PilotList from "../PilotList/PilotList.jsx";
+// import clsx from "clsx";
+// import { FaBeer } from "react-icons/fa";
 // import css from "./App.module.css";
+
+import { BsBoxArrowInDownLeft } from "react-icons/bs";
 
 // const activeOfficers = officers.filter((officer) => officer.active);
 
 // const youngOfficers = officers.filter((officer) => officer.age < 40);
 
+const Box = (props) => {
+  console.log(props);
+
+  return (
+    <section>
+      <h1>Hello</h1>
+      {props.children}
+    </section>
+  );
+};
 export default function App() {
   return (
     <>
-      {/* <h1 className={css.title}>Hello world</h1> */}
+      <Box>
+        <button> f</button>
+        <a href="">12345</a>
+      </Box>
+      {/* <FaBeer />
+      
       <AppBar />
       <h2>Active Officers</h2>
       <PilotList items={officers} />
--- src/components/PilotProfile/PilotProfile.jsx
+++ src/components/PilotProfile/PilotProfile.jsx
@@ -1,5 +1,7 @@
 import css from "./PilotProfile.module.css";
-
+import { BsAirplaneFill } from "react-icons/bs";
+import { BsAlarmFill } from "react-icons/bs";
+import { BsChatFill } from "react-icons/bs";
 export default function PilotProfile({
   pilot: { name, rank, age, spec, active },
 }) {
@@ -9,10 +11,19 @@ export default function PilotProfile({
   return (
     <div>
       <p className={css.text}>Name: {name}</p>
-      <p className={css.text}>Rank: {rank}</p>
+      <p className={css.text}>
+        <BsAirplaneFill className={css.icon} /> Rank: {rank}
+      </p>
       <p className={css.text}>Age: {age}</p>
-      <p className={css.text}>Specialty: {spec}</p>
-      <p className={css.text}>Status: {active ? "Active" : "Retired"}</p>
+      <p className={css.text}>
+        <BsAlarmFill className={css.icon} />
+        Specialty: {spec}
+      </p>
+      <p className={css.text}>
+        {" "}
+        <BsChatFill className={css.icon} />
+        Status: {active ? "Active" : "Retired"}
+      </p>
     </div>
   );
 }
